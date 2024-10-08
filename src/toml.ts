d987ff6f97dc7186ca0b224392da74567389264c
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-10-08 04:39:55 +0000

    working on toml parser

--- src/toml.ts
+++ src/toml.ts
@@ -1,9 +1,17 @@
 import { Context, Effect, Layer } from "effect";
+import TOML, { type TomlPrimitive } from "smol-toml";
 
-type ITomlClient = {};
+type ITomlClient = Readonly<{
+	parse: (value: string) => Record<string, TomlPrimitive>;
+}>;
 
 const make = Effect.gen(function* () {
-	return {} as const;
+	const parse = (value: string) => TOML.parse(value);
+	const encode = (value: TomlPrimitive) => TOML.stringify(value);
+
+	return {
+		parse,
+	} as const satisfies ITomlClient;
 });
 
 export class TomlClient extends Context.Tag("toml-client")<
