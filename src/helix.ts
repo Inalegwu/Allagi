d1f63c6d2c9c7d87788f40609a1841d3e6df4e27
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-10-09 13:40:42 +0000

    remove option

--- src/helix.ts
+++ src/helix.ts
@@ -1,13 +1,12 @@
 import { Args, Command } from "@effect/cli";
-// biome-ignore lint/suspicious/noShadowRestrictedNames: useful
-import { Effect, Option, Array } from "effect";
+import { Effect, Array } from "effect";
 import { VSCodeTheme } from "./schema.vs";
 import { Schema } from "@effect/schema";
 import { convertHexToRGB, determineColorSpace } from "./utils";
 
 const vscodeThemePath = Args.path({
 	name: "VSCode Theme Path",
-}).pipe(Args.optional);
+});
 
 const outputPath = Args.path({
 	name: "Helix Theme Output Path",
@@ -18,44 +17,40 @@ const toHelix = Command.make(
 	{ vscodeThemePath, outputPath },
 	({ vscodeThemePath, outputPath }) =>
 		Effect.gen(function* () {
-			yield* Option.match(vscodeThemePath, {
-				onSome: (themePath) =>
-					Effect.gen(function* () {
-						const file = yield* Effect.tryPromise(() =>
-							Bun.file(themePath).text(),
-						);
+			yield* Effect.logInfo(
+				`Attempting to Convert ${vscodeThemePath} to Helix Theme @${outputPath}`,
+			);
+			const file = yield* Effect.tryPromise(() =>
+				Bun.file(vscodeThemePath).text(),
+			);
 
-						const vscodeSchema = yield* Schema.decodeUnknown(VSCodeTheme)(
-							JSON.parse(file),
-							{
-								onExcessProperty: "ignore",
-							},
-						);
+			const vscodeSchema = yield* Schema.decodeUnknown(VSCodeTheme)(
+				JSON.parse(file),
+				{
+					onExcessProperty: "ignore",
+				},
+			);
 
-						const colors = Array.fromRecord(vscodeSchema.colors)
-							.map((v) => ({
-								key: v[0],
-								rgb: convertHexToRGB(v[1]),
-								hex: v[1],
-							}))
-							.map((value) => ({
-								key: value.key,
-								rgb: value.rgb,
-								hex: value.hex,
-								colorSpace: determineColorSpace({ ...value.rgb }),
-							}));
+			const colors = Array.fromRecord(vscodeSchema.colors)
+				.map((v) => ({
+					key: v[0],
+					rgb: convertHexToRGB(v[1]),
+					hex: v[1],
+				}))
+				.map((value) => ({
+					key: value.key,
+					rgb: value.rgb,
+					hex: value.hex,
+					colorSpace: determineColorSpace({ ...value.rgb }),
+				}));
 
-						const background = colors
-							.filter((value) => value.key === "editor.background")
-							.at(0);
+			const background = colors
+				.filter((value) => value.key === "editor.background")
+				.at(0);
 
-						// yield* Console.log(schema);
-					}),
-				onNone: () =>
-					Effect.gen(function* () {
-						yield* Effect.logError("Incomplete Arguments Provided");
-					}),
-			});
+			yield* Effect.logInfo({ background });
+
+			// yield* Console.log(schema);
 		}).pipe(
 			Effect.tapError((error) => Effect.logError(error)),
 			Effect.annotateLogs({
