0e4b8aef8c8b4d4319c1f8d87f78b69b5a33fda3
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-10-09 16:47:05 +0000

    remove need for output file path, generate name based on discovered theme name

--- src/helix.ts
+++ src/helix.ts
@@ -12,58 +12,58 @@ const inputPath = Args.path({
 	name: "Theme Path",
 });
 
-const outputPath = Args.path({
-	name: "Output Path",
-});
+const helix = Command.make("helix", { inputPath }, ({ inputPath }) =>
+	Effect.gen(function* () {
+		yield* Effect.logInfo(`Attempting to Convert ${inputPath} to Helix Theme`);
+		const file = yield* Effect.tryPromise(() => Bun.file(inputPath).text());
 
-const helix = Command.make(
-	"helix",
-	{ inputPath, outputPath },
-	({ inputPath, outputPath }) =>
-		Effect.gen(function* () {
-			yield* Effect.logInfo(
-				`Attempting to Convert ${inputPath} to Helix Theme @${outputPath}`,
-			);
-			const file = yield* Effect.tryPromise(() => Bun.file(inputPath).text());
+		yield* Effect.logInfo("Reading Theme File");
+		const vscodeSchema = yield* Schema.decodeUnknown(VSCodeTheme)(
+			JSON.parse(file),
+			{
+				onExcessProperty: "ignore",
+			},
+		);
 
-			yield* Effect.logInfo("Reading Theme File");
-			const vscodeSchema = yield* Schema.decodeUnknown(VSCodeTheme)(
-				JSON.parse(file),
-				{
-					onExcessProperty: "ignore",
-				},
-			);
+		yield* Effect.logInfo(
+			`Converting ${vscodeSchema.name} by ${vscodeSchema.author}`,
+		);
 
-			yield* Effect.logInfo(
-				`Converting ${vscodeSchema.name} by ${vscodeSchema.author}`,
-			);
+		yield* Effect.logInfo("Discovering Palette");
+		const palette = Array.fromRecord(vscodeSchema.colors)
+			.map((v) => ({
+				key: v[0],
+				rgb: convertHexToRGB(v[1]),
+				hex: v[1],
+			}))
+			.map((value) => ({
+				key: value.key,
+				rgb: value.rgb,
+				hex: value.hex,
+				colorSpace: determineColorSpace({ ...value.rgb }),
+			}));
 
-			yield* Effect.logInfo("Discovering Palette");
-			const palette = Array.fromRecord(vscodeSchema.colors)
-				.map((v) => ({
-					key: v[0],
-					rgb: convertHexToRGB(v[1]),
-					hex: v[1],
-				}))
-				.map((value) => ({
-					key: value.key,
-					rgb: value.rgb,
-					hex: value.hex,
-					colorSpace: determineColorSpace({ ...value.rgb }),
-				}));
+		yield* Effect.logInfo("Successfully Discovered Palette");
+		yield* Effect.logInfo(
+			`Preparing to convert and save to ${vscodeSchema.name.toLowerCase().split(" ").join("_")}.toml`,
+		);
 
-			const reds = palette.filter((color) => color.colorSpace === "red");
-			const greens = palette.filter((color) => color.colorSpace === "green");
-			const blues = palette.filter((color) => color.colorSpace === "blue");
+		// const reds = palette.filter((color) => color.colorSpace === "red");
+		// const greens = palette.filter((color) => color.colorSpace === "green");
+		// const blues = palette.filter((color) => color.colorSpace === "blue");
 
-			// yield* Console.log(schema);
-		}).pipe(
-			Effect.tapError((error) =>
-				Effect.logError(
-					`Error occured converting VSCode Theme to Helix ${error}`,
-				),
+		// yield* Console.log(schema);
+	}).pipe(
+		Effect.tapError((error) =>
+			Effect.logError(
+				`Error occured converting VSCode Theme to Helix ${error}`,
+			).pipe(
+				Effect.annotateLogs({
+					command: "helix",
+				}),
 			),
 		),
+	),
 );
 
 export default helix;
